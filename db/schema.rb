f4135753f76070d3a23a3bf15004a42983061f40
Erik <erik.steinman@hotmail.com>
2018-09-20 09:35:43 +0200

    add acts_as_follower to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_072155) do
+ActiveRecord::Schema.define(version: 2018_09_19_084658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,20 @@ ActiveRecord::Schema.define(version: 2018_09_15_072155) do
     t.bigint "genre_id"
     t.index ["campaign_id"], name: "index_campaigns_genres_on_campaign_id"
     t.index ["genre_id"], name: "index_campaigns_genres_on_genre_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.string "followable_type", null: false
+    t.bigint "followable_id", null: false
+    t.string "follower_type", null: false
+    t.bigint "follower_id", null: false
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followable_id", "followable_type"], name: "fk_followables"
+    t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
+    t.index ["follower_id", "follower_type"], name: "fk_follows"
+    t.index ["follower_type", "follower_id"], name: "index_follows_on_follower_type_and_follower_id"
   end
 
   create_table "genres", force: :cascade do |t|
